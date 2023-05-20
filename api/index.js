ff85f9020dc9e22768e173a64a32b0e142aabb99
Sophia Liu <sophialiu7436@gmail.com>
2023-05-20 17:10:48 -0500

    Configured API registration endpoint (1:00)

--- api/index.js
+++ api/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config()
 
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+// o0b610rqh6GCCM1T
+
 app.get('/test', (req,res) => {
     res.json('test ok');
+});
+
+app.post('/register', (req,res)=> {
+    const {name,email,password} = req.body;
+    
+    res.json({name,email,password});
 });
 
 app.listen(4000);
