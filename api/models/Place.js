15d45acbcf2cb61f2484280c0511aeae65c09be4
Sophia Liu <sophialiu7436@gmail.com>
2023-05-23 12:57:03 -0500

    Completed accomodation upload form

---
+++ api/models/Place.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+    owner: {type:mongoose.Schema.Types.ObjectId, ref:'User'}, // ID of a user
+    title: String,
+    address: String,
+    photos: [String], // Array of strings
+    description: String,
+    perks: [String],
+    extraInfo: String,
+    checkIn: Number,
+    checkOut: Number,
+    maxGuests: Number,
+});
+
+// Create place model
+const PlaceModel = mongoose.model('Place', placeSchema)
+
+module.exports = PlaceModel; // Now we can use our model
